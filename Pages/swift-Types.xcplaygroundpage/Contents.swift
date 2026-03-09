e32365245ca51f9d615d930bee8a8d611158d072
Khalid Abdi <xamza2009@gmail.com>
2026-03-09 21:30:28 +0300

    learn data types

---
+++ Pages/swift-Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var name = "k"
+var num:String = "kahdli"
+var k:Bool = false
+
+let mydate:Date = .now
+print(mydate)
+
+let Ints:Double = 100
+//: [Next](@next)
