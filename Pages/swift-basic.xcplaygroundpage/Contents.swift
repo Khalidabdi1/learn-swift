4387e7639278c9737d2c492d3e20d47bf8bff931
Khalid Abdi <xamza2009@gmail.com>
2026-03-09 21:01:58 +0300

    start

---
+++ Pages/swift-basic.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "khalid"
+/*
+ names
+ */
+print(greeting+" abdi")
+
+let name = "khalid"
+greeting="kalib"
+
+print(greeting)
+
+//: [Next](@next)
