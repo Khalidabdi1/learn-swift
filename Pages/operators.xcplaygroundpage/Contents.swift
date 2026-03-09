24660dc3fc2dcc15933122f069ba16b83c632408
Khalid Abdi <xamza2009@gmail.com>
2026-03-09 22:14:28 +0300

    and or

---
+++ Pages/operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+var likeCount:Double = 5
+var commentCount:Double = 0
+
+var viewCount:Double = 100
+
+if (likeCount == 5) && (commentCount==0){
+    print("yes, sir")
+}
+
+likeCount += 1
+
+print(likeCount)
+
+
+commentCount *= 1
+print(commentCount)
+
+
+
+viewCount /= 2
+print(viewCount)
+
+
+
+
+
+
+//: [Next](@next)
